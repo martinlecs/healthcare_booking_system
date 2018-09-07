0000f3eac541fdaf9e8fdfd9925eb7e1e7143e9f
Aran Pando <gbye45@hotmail.com>
2018-09-07 11:56:42 +1000

    User class skeleton, to start building patients. Authentication attributes and methods will be added when i start to implement login system

---
+++ user.py
@@ -0,0 +1,30 @@
+from abc import ABC 
+
+class User(ABC):
+	"Abstract user class"
+	def __init__(self, email, password):
+		self._email = email
+		self._password = password
+
+	@property
+	def email(self):
+		return self._email
+	
+	@property
+	def password(self):
+		return self._password
+	
+	@email.setter
+	def email(self, email):
+		self._email = email
+
+	@password.setter
+	def password(self, pwd):
+		self._password = pwd
+
+	# authentication functions #
+	#
+	#
+	#
+	# authentication functions #
+
