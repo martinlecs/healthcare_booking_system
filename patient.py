25d9af93e14f65b7143baabba3d2b67d98cb6db8
Natalie Nigro <z5229588@student.unsw.edu.au>
2018-09-07 16:29:13 +1000

    both patient and provider classes with basic strucutre based on parent class User

---
+++ patient.py
@@ -0,0 +1,32 @@
+class Patient(User):
+	"Patient class"
+	def __init__(self, email, password, surname, givenName, medicareNo, appointments):
+		user = User(self, email, password)
+        self._surname = surname
+        self._givenName = givenName
+        self._medicareNo = medicareNo
+        self._appointments = appointments
+
+	@property
+	def surname(self):
+		return self._surname
+
+	@property
+	def givenName(self):
+		return self._givenName
+
+	@property
+	def medicareNo(self):
+		return self._medicareNo
+
+	@property
+	def appointments(self):
+		return self._appointments
+
+	@medicareNo.setter
+    def medicareNo(self, medicareNo):
+        self._medicareNo = medicareNo
+
+	@appointments.setter
+	def appointments(self, appointments):
+		self._appointments = appointments
