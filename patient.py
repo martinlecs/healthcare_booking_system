16db71d176b9fecd0b35c10d15fe0d765676d245
Natalie Nigro <z5229588@student.unsw.edu.au>
2018-09-10 15:44:42 +1000

    moved view fns to user

--- patient.py
+++ patient.py
@@ -13,17 +13,3 @@ class Patient(User):
 	@medicare_no.setter
 	def medicare_no(self, medicare_no):
 		self._medicare_no = medicare_no
-
-	def view_past_appointments(self):
-		past_appointments = []
-		for appt in appointments:
-			if appt.past == true:
-				past_appointments.append(appt)
-		return past_appointments
-
-	def view_current_appointments(self):
-		cur_appointments = []
-		for appt in appointments:
-			if appt.past == false:
-				cur_appointments.append(appt)
-		return cur_appointments
