bfe1da1fcb8bf52b59f35d91a519f9075f85c723
Aran Pando <gbye45@hotmail.com>
2018-09-10 12:59:12 +1000

    Added get past and upcoming appts

--- user.py
+++ user.py
@@ -2,9 +2,12 @@ from flask_login import UserMixin
 
 class User(UserMixin):
 	"User class"
-	def __init__(self, email, password):
+	def __init__(self, email, password, surname, given_name, appointments=[]):
 		self._email = email
 		self._password = password
+		self._surname = surname
+		self._given_name = given_name
+		self._appointments = appointments
 
 	@property
 	def email(self):
@@ -13,7 +16,20 @@ class User(UserMixin):
 	@property
 	def password(self):
 		return self._password
+
+	@property
+	def surname(self):
+		return self._surname
 	
+	@property
+	def given_name(self):
+		return self._given_name
+
+	@property
+	def appointments(self):
+		return self._appointments
+	
+
 	@email.setter
 	def email(self, email):
 		self._email = email
@@ -21,6 +37,37 @@ class User(UserMixin):
 	@password.setter
 	def password(self, pwd):
 		self._password = pwd
+
+	@surname.setter
+	def surname(self, surname):
+		self._surname = surname
+
+	@given_name.setter
+	def given_name(self, given_name):
+		self._given_name = given_name
+
+	def add_appointment(self, appt_obj):
+		if not any(appointment.appointment_id == appt_obj.appointment_id for appointment in self._appointments):
+			self._appointments.append(appt_obj)
+
+	def remove_appointment_by_id(self, appt_id):
+		for i, appt in enumerate(self._appointments):
+			if appt.appointment_id == appt_id
+			del self._appointments[i]
+
+	def get_past_appointments(self):
+		past_appts = []
+		for appt in self._appointments:
+			if appt.past == True:
+				past_appts.append(appt)
+		return past_appts
+
+	def get_upcoming_appointments(self):
+		upcoming_appts = []
+		for appt in self._appointments:
+			if appt.past == False:
+				upcoming_appts.append(appt)
+		return upcoming_appts
 
 	def get_id(self):
 		return self._email
