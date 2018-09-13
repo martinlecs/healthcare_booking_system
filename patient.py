a2f4c09b85dc4438a7e714bc2daad19723687c32
Aran Pando <gbye45@hotmail.com>
2018-09-13 18:37:05 +1000

    little fix up

--- patient.py
+++ patient.py
@@ -13,38 +13,3 @@ class Patient(User):
 	@medicare_no.setter
 	def medicare_no(self, medicare_no):
 		self._medicare_no = medicare_no
-
-# from user import User
-
-# class Patient(User):
-# 	"Patient class"
-# 	def __init__(self, email, password, surname, givenName, medicareNo, appointments=[]):
-# 		super().__init__(email, password)
-# 		self._surname = surname
-# 		self._givenName = givenName
-# 		self._medicareNo = medicareNo
-# 		self._appointments = appointments
-
-# 	@property
-# 	def surname(self):
-# 		return self._surname
-
-# 	@property
-# 	def givenName(self):
-# 		return self._givenName
-
-# 	@property
-# 	def medicareNo(self):
-# 		return self._medicareNo
-
-# 	@property
-# 	def appointments(self):
-# 		return self._appointments
-
-# 	@medicareNo.setter
-# 	def medicareNo(self, medicareNo):
-# 		self._medicareNo = medicareNo
-
-# 	@appointments.setter
-# 	def appointments(self, appointments):
-# 		self._appointments = appointments
