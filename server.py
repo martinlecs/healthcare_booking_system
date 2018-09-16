43376890ec58a2552e4e218ad28dded4d16be186
Jon DUFTY <j.dufty@student.unsw.edu.au>
2018-09-17 00:54:13 +1000

    removed systemManager class

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from centre_manager import CentreManager
 from user_manager import UserManager
 from appointment_manager import AppointmentManager
-from system import SystemManager
+from system import link_centre_provider
 
 
 # Instantiate 'global' variables
@@ -12,5 +12,5 @@ app.secret_key = 'super secret shhhh'
 
 user_manager = UserManager.load_data()
 centre_manager = CentreManager.load_data()
+link_centre_provider(centre_manager, user_manager)
 appt_manager = AppointmentManager.load_data()
-system = SystemManager(user_manager, centre_manager)
