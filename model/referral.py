c822d974784f38cb6e57d3e4f037dcb52fa5cca6
Jon DUFTY <j.dufty@student.unsw.edu.au>
2018-10-10 21:21:07 +1100

    removed css temporarily to allow others to test

---
+++ model/referral.py
@@ -0,0 +1,6 @@
+class Referral:
+    def __init__(self, patient, gp, specialist, msg):
+        self._patient = patient
+        self._gp = gp
+        self._specialist = specialist
+        self._msg = msg
