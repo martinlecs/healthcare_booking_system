2a1a5f54f81896b72bf1433b069dc939703fcefb
Aran Pando <gbye45@hotmail.com>
2018-10-14 20:58:09 +1100

    completed test book appt

--- test_provider.py
+++ test_provider.py
@@ -18,140 +18,140 @@ from model.date_validity import *
 #	get availability (3)
 # 	make time slot unavailable (2)
 
-# def test_getters():
-# 	p = Provider('test@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
-# 	info = p.get_information
-# 	assert(info["email"] == 'test@gmail.com'.lower())
-# 	assert(info["password"]	== '1234'.lower())
-# 	assert(info["surname"] 	== 'McTester'.lower())
-# 	assert(info["given_name"] == 'Test'.lower())
-# 	assert(info["provider_no"] == '124024114'.lower())
-# 	assert(info["service"] == 'Official Tester'.lower())
-# 	assert(info["appointments"] == [])
-# 	assert(info["centres"] == [])
-# 	assert(info["availability"] == {})
-# 	assert(info["rating"] == {})
-# 	assert(info["average_rating"] == 0)
+def test_getters():
+	p = Provider('test@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
+	info = p.get_information
+	assert(info["email"] == 'test@gmail.com'.lower())
+	assert(info["password"]	== '1234'.lower())
+	assert(info["surname"] 	== 'McTester'.lower())
+	assert(info["given_name"] == 'Test'.lower())
+	assert(info["provider_no"] == '124024114'.lower())
+	assert(info["service"] == 'Official Tester'.lower())
+	assert(info["appointments"] == [])
+	assert(info["centres"] == [])
+	assert(info["availability"] == {})
+	assert(info["rating"] == {})
+	assert(info["average_rating"] == 0)
 
-# def test_setters():
-# 	p = Provider('test@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
+def test_setters():
+	p = Provider('test@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
 	
-# 	p.email = 'TESting@gmail.Com'
-# 	info = p.get_information
-# 	assert(info['email'] == 'testing@gmail.com' )
+	p.email = 'TESting@gmail.Com'
+	info = p.get_information
+	assert(info['email'] == 'testing@gmail.com' )
 	
-# 	p.password = 'WoooWThatsGreat'
-# 	info = p.get_information
-# 	assert(info['password'] != 'wooowthatsgreat' and info['password'] == 'WoooWThatsGreat')
+	p.password = 'WoooWThatsGreat'
+	info = p.get_information
+	assert(info['password'] != 'wooowthatsgreat' and info['password'] == 'WoooWThatsGreat')
 	
-# 	p.surname = 'MCTESTing'
-# 	info = p.get_information
-# 	assert(info['surname'] == 'mctesting')
+	p.surname = 'MCTESTing'
+	info = p.get_information
+	assert(info['surname'] == 'mctesting')
 	
-# 	p.given_name = 'TEEEESt'
-# 	info = p.get_information
-# 	assert(info['given_name'] == 'teeeest')
+	p.given_name = 'TEEEESt'
+	info = p.get_information
+	assert(info['given_name'] == 'teeeest')
 	
-# 	p.provider_no = '123123'
-# 	info = p.get_information
-# 	assert(info['provider_no'] == '123123')
+	p.provider_no = '123123'
+	info = p.get_information
+	assert(info['provider_no'] == '123123')
 	
-# 	p.service = 'OFFicial TESTer'
-# 	info = p.get_information
-# 	assert(info['service'] == 'official tester')
+	p.service = 'OFFicial TESTer'
+	info = p.get_information
+	assert(info['service'] == 'official tester')
 
 
-# def test_add_non_existing_centre():
-# 	p1 = Provider('test@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
-# 	assert(p1.centres == [])
-# 	p1.add_centre('Randwick Hospital')
-# 	assert('Randwick Hospital'.lower() in p1.centres)
+def test_add_non_existing_centre():
+	p1 = Provider('test@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
+	assert(p1.centres == [])
+	p1.add_centre('Randwick Hospital')
+	assert('Randwick Hospital'.lower() in p1.centres)
 
-# def test_add_existing_centre():
-# 	pp1 = Provider('trst','123', 'sad', '12312', '2323', 'Blah')
-# 	assert(pp1.centres == [])
-# 	pp1.add_centre('Randwick Hospital')
-# 	pp1.add_centre('randwick hospital')
-# 	assert(len(pp1.centres) == 1)
-
-
-# def test_remove_non_existing_centre():
-# 	prov = Provider('testsssss@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
-# 	assert(prov.email == 'testsssss@gmail.com')
-# 	assert(prov.centres == [])
-# 	checker = prov.remove_centre('Randwick Hospital')
-# 	assert(checker is False)
-
-# def test_remove_existing_centre():
-# 	prov = Provider('testsssss@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
-# 	assert(prov.centres == [])
-# 	prov.add_centre('rand')
-# 	assert('rand' in prov.centres)
-# 	checker = prov.remove_centre('RAnd')
-# 	assert(checker == True)
-# 	assert(prov.centres == [])
+def test_add_existing_centre():
+	pp1 = Provider('trst','123', 'sad', '12312', '2323', 'Blah')
+	assert(pp1.centres == [])
+	pp1.add_centre('Randwick Hospital')
+	pp1.add_centre('randwick hospital')
+	assert(len(pp1.centres) == 1)
 
 
-# def test_add_int_between_0_and_5_rating():
-# 	p = Provider('1','1','1','','','')
-# 	p.add_rating('patient@gmail.com',3)
-# 	assert('patient@gmail.com' in p.rating.keys())
-# 	assert(p.rating['patient@gmail.com'] == 3)
+def test_remove_non_existing_centre():
+	prov = Provider('testsssss@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
+	assert(prov.email == 'testsssss@gmail.com')
+	assert(prov.centres == [])
+	checker = prov.remove_centre('Randwick Hospital')
+	assert(checker is False)
 
-# def test_add_int_NOT_between_0_and_5_rating():
-# 	p = Provider('1','1','1','','','')
-# 	assert(list(p.rating.keys()) == [])
-# 	checker = p.add_rating('patient@gmail.com',-1)
-# 	assert(checker == False)
-# 	assert(list(p.rating.keys()) == [])
-
-# def test_add_string_rating():
-# 	p = Provider('1','1','1','','','')
-# 	assert(list(p.rating.keys()) == [])
-# 	checker = p.add_rating('patient@gmail.com','1')
-# 	assert(checker == False)
-# 	assert(list(p.rating.keys()) == [])
-
-# def test_add_rating_for_existing_patient():
-# 	p = Provider('1','1','1','','','')
-# 	p.add_rating('patient@gmail.com',3)
-# 	assert(p.rating['patient@gmail.com'] == 3)
-# 	p.add_rating('patient@gmail.com',5)
-# 	assert(p.rating['patient@gmail.com'] == 5)	
+def test_remove_existing_centre():
+	prov = Provider('testsssss@gmail.com','1234','McTester', 'Test','124024114', 'Official Tester')
+	assert(prov.centres == [])
+	prov.add_centre('rand')
+	assert('rand' in prov.centres)
+	checker = prov.remove_centre('RAnd')
+	assert(checker == True)
+	assert(prov.centres == [])
 
 
-# def test_remove_rating_of_existing_patient():
-# 	p = Provider('1','1','1','','','')
-# 	p.add_rating('patient@gmail.com',3)
-# 	checker = p.remove_rating('patient@gmail.com')
-# 	assert(checker == True)
-# 	assert(list(p.rating.keys()) == [])
+def test_add_int_between_0_and_5_rating():
+	p = Provider('1','1','1','','','')
+	p.add_rating('patient@gmail.com',3)
+	assert('patient@gmail.com' in p.rating.keys())
+	assert(p.rating['patient@gmail.com'] == 3)
 
-# def test_remove_rating_of_non_existing_patient():
-# 	p = Provider('1','1','1','','','')
-# 	checker = p.remove_rating('patient@gmail.com')
-# 	assert(checker == False)
-# 	assert(list(p.rating.keys()) == [])
+def test_add_int_NOT_between_0_and_5_rating():
+	p = Provider('1','1','1','','','')
+	assert(list(p.rating.keys()) == [])
+	checker = p.add_rating('patient@gmail.com',-1)
+	assert(checker == False)
+	assert(list(p.rating.keys()) == [])
+
+def test_add_string_rating():
+	p = Provider('1','1','1','','','')
+	assert(list(p.rating.keys()) == [])
+	checker = p.add_rating('patient@gmail.com','1')
+	assert(checker == False)
+	assert(list(p.rating.keys()) == [])
+
+def test_add_rating_for_existing_patient():
+	p = Provider('1','1','1','','','')
+	p.add_rating('patient@gmail.com',3)
+	assert(p.rating['patient@gmail.com'] == 3)
+	p.add_rating('patient@gmail.com',5)
+	assert(p.rating['patient@gmail.com'] == 5)	
 
 
-# def test_calc_average_rating_with_1_rating():
-# 	p = Provider('1','1','1','','','')
-# 	p.add_rating('patient@gmail.com',3)
-# 	assert(p.average_rating == 3)
+def test_remove_rating_of_existing_patient():
+	p = Provider('1','1','1','','','')
+	p.add_rating('patient@gmail.com',3)
+	checker = p.remove_rating('patient@gmail.com')
+	assert(checker == True)
+	assert(list(p.rating.keys()) == [])
 
-# def test_calc_average_rating_with_more_than_1_rating():
-# 	p = Provider('1','1','1','','','')
-# 	p.add_rating('patient@gmail.com',3)
-# 	p.add_rating('222patient@gmail.com',2)
-# 	p.add_rating('pat333ient@gmail.com',5)
-# 	assert(p.average_rating == (3+2+5)/(float(3)))
+def test_remove_rating_of_non_existing_patient():
+	p = Provider('1','1','1','','','')
+	checker = p.remove_rating('patient@gmail.com')
+	assert(checker == False)
+	assert(list(p.rating.keys()) == [])
 
-# def test_calc_average_rating_with_out_ratings():
-# 	p = Provider('1','1','1','','','')
-# 	assert(p.average_rating == 0)
-# 	p.add_rating('patient@gmail.com',3)
-# 	p.remove_rating('patient@gmail.com')
-# 	assert(p.average_rating == 0)
+
+def test_calc_average_rating_with_1_rating():
+	p = Provider('1','1','1','','','')
+	p.add_rating('patient@gmail.com',3)
+	assert(p.average_rating == 3)
+
+def test_calc_average_rating_with_more_than_1_rating():
+	p = Provider('1','1','1','','','')
+	p.add_rating('patient@gmail.com',3)
+	p.add_rating('222patient@gmail.com',2)
+	p.add_rating('pat333ient@gmail.com',5)
+	assert(p.average_rating == (3+2+5)/(float(3)))
+
+def test_calc_average_rating_with_out_ratings():
+	p = Provider('1','1','1','','','')
+	assert(p.average_rating == 0)
+	p.add_rating('patient@gmail.com',3)
+	p.remove_rating('patient@gmail.com')
+	assert(p.average_rating == 0)
 	
 def test_get_availability_invalid_date():
 	p = Provider('1','1','1','','','')
@@ -161,7 +161,6 @@ def test_get_availability_invalid_date():
 		checker = p.get_availability('centre', 'lol', 22, 414)
 		
 	
-
 # def test_get_availability_for_non_existing_centre_and_non_existing_but_valid_date():
 # 	p = Provider('1','1','1','','','')
 # 	# p.add_centre('rand')
