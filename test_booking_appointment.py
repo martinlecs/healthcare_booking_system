39f5b9cd9afe804305858c9341ea7de86a11ce82
Aran Pando <gbye45@hotmail.com>
2018-10-14 20:44:06 +1100

    Completed test booking appt

--- test_booking_appointment.py
+++ test_booking_appointment.py
@@ -5,8 +5,9 @@ from model.appointment_manager import AppointmentManager
 from model.provider import Provider
 from model.patient import Patient
 from model.appointment import Appointment
+from model.centre import Centre
 from model.error import IdentityError, BookingError, DateTimeValidityError
-from model.date_validity import date_and_time_valid, date_validity
+from model.date_validity import date_and_time_valid, date_valid
 from datetime import date, datetime, time
 
 '''
@@ -81,26 +82,42 @@ def test_invalid_date(um_fixture):
 		provider.get_availability(provider.centres[0], 19922, 13, 2222)
 	with pytest.raises(DateTimeValidityError) as info:
 		provider.get_availability(provider.centres[0], 'lol', 12, 12)
-
-# test_valid_date_and_time_slot():
-
+	avail = provider.get_availability(provider.centres[0], 2019, 10, 15)
+	assert(type(avail) is list)
 
 # ****Provider book with self**** #
 
-# test_provider_book_with_self():
-
+def test_provider_book_with_self(um_fixture):
+	am = AppointmentManager()
+	with pytest.raises(BookingError) as info:
+		am.make_appt_and_add_appointment_to_manager('prov@gmail.com', 'prov@gmail.com', 'centre_id', '2019-08-09', '09:30', 'reason')
 
 # ****Valid Booking**** #
+# Test it makes appt, stores values, stores appt in appt_manager
+def test_valid_booking(um_fixture):
+	am = AppointmentManager()
+	appt = am.make_appt_and_add_appointment_to_manager('patient@gmail.com', 'prov@gmail.com', 'centre_id', '2019-08-09', '09:30', 'reason')
+	assert(type(appt) == Appointment)
+	assert(appt.patient_email == 'patient@gmail.com')
+	assert(appt.provider_email == 'prov@gmail.com')
+	assert(appt.centre_id == 'centre_id')
+	assert(appt.date == '2019-08-09')
+	assert(appt.time_slot == '09:30')
+	assert(appt.reason == 'reason')
+	assert(appt in am.appointments)
 
-# **Valid Provider and Centre** #
+
+# ****Valid Provider and Centre**** #
 
 # test_non_existing_provider():
+	# This is taken care of in routes so it can't be tested through pytest
+	# However, I promise you it works. Test it.
 
 # test_non_existing_centre():
+	# This is taken care of in routes so it can't be tested...
+	# However, I promise you it works. Test it.
 
 # test_existing_centre_and_provider():
-
-# **Valid reason** #
-
-# test_valid_reason():
+	# This is taken care of in routes so it can't be tested through pytest
+	# However, I promise you it works. Test it.
 
